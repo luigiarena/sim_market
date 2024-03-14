e65557ad111d6334f8a032833f558ed1f8a845e2
luigiarena <luigiarena.cs@gmail.com>
2024-03-14 18:43:22 +0100

    Aggiunto commento alla struttura delle casse

--- include/cassa.h
+++ include/cassa.h
@@ -1,3 +1,4 @@
+/* Struttura che definisce l'oggetto Cassa */
 typedef struct Cassa {
     long id;        // identificativo della cassa
     int stato;      // booleano, 1 -> cassa aperta, 0 -> cassa chiusa
