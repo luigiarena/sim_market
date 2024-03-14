109bcdfcf402a9d81c9f8a133d07e04529130823
luigiarena <luigiarena.cs@gmail.com>
2024-03-14 18:15:12 +0100

    Creazione array di casse

--- src/supermercato.c
+++ src/supermercato.c
@@ -4,9 +4,15 @@
 
 #include "supermercato.h"
 #include "utils.h"
+#include "cassa.h"
+
+int K;
+Cassa *casse_list;
 
 int main(int argc, char* argv[]){
 
+    // Creazione casse
+    casse_list = (Cassa*) malloc(sizeof(Cassa)*K);
 
 
 }
