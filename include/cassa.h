13266f84d6c668ba1bea5aba2d854b8202a2e90b
luigiarena <luigiarena.cs@gmail.com>
2024-05-17 12:10:58 +0200

    Aggiunta di alcune variabili nella struttura delle casse

--- include/cassa.h
+++ include/cassa.h
@@ -6,8 +6,14 @@
 
 /* Struttura che definisce l'oggetto Cassa */
 typedef struct Cassa {
-    long id;        // identificativo della cassa
-    int stato;      // booleano, 1 -> cassa aperta, 0 -> cassa chiusa
+    long id;            // identificativo della cassa
+    int stato;          // booleano, 1 -> cassa aperta, 0 -> cassa chiusa
+
+    int t_servizio;     // tempo di servizio definito per servire un cliente
+    int num_clienti;    // numero totale di clienti serviti
+    int num_prodotti;   // numero totale di prodotti scansionati
+    int chiusure;       // numero di chiusure della cassa
+
 } Cassa;
 
 void* cassa_main(void* arg);
