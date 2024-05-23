cc7b5a98bdb370523db7296937e5e28c0c654456
luigiarena <luigiarena.cs@gmail.com>
2024-05-23 10:58:05 +0200

    Aggiunto punto e virgola mancante in cliente.h

--- include/cliente.h
+++ include/cliente.h
@@ -8,7 +8,7 @@
 typedef struct Cliente {
     long id;            // identificativo del cliente
 
-    int t_spesa         // tempo occupato dal cliente per fare la spesa
+    int t_spesa;         // tempo occupato dal cliente per fare la spesa
     int t_cassa;        // tempo che il cliente spende alla cassa
     int num_prodotti;   // numero di prodotti acquistati
 
